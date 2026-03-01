d8f8a7c19569a9556682963f99c2118b2ba1615e
0somesh <0somesh.sh@gmail.com>
2026-03-01 15:35:14 +0530

    update file

--- anagramGroup.py
+++ anagramGroup.py
@@ -1,15 +1,15 @@
 from collections import defaultdict
 
-def get_anagrams(word_list):
+def get_anagram(word_list):
     """The logic to group words by their sorted characters."""
-    anagrams = defaultdict(list)
+    anagram = defaultdict(list)
     for word in word_list:
 
         key = ''.join(sorted(word))
-        anagrams[key].append(word)
-    return list(anagrams.values())
+        anagram[key].append(word)
+    return list(anagram.values())
 
-def start_anagram_session():
+def start_anagram():
     """Recursive function to handle user input and output."""
     
     #i/p
@@ -21,10 +21,9 @@ def start_anagram_session():
         print("end program")
         return 
 
-    
     if user_input:
         word_list = user_input.split()
-        result = get_anagrams(word_list)
+        result = get_anagram(word_list)
         
         #o/p
         print(f"Grouped Anagrams: {result}")
@@ -32,6 +31,6 @@ def start_anagram_session():
         print("Empty string")
 
     #recursion
-    start_anagram_session()
+    start_anagram()
 
-start_anagram_session()
+start_anagram()
