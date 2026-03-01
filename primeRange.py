fbc14cb1f6c23991a1c8b081c30ffa9ce26650ef
0somesh <0somesh.sh@gmail.com>
2026-03-01 14:56:37 +0530

    update

--- primeRange.py
+++ primeRange.py
@@ -1,12 +1,10 @@
 def is_prime(num):
     if num <= 1:
         return False
-    
     for i in range(2, int(num ** 0.5) + 1):
         if num % i == 0:
             return False
     return True
-
 
 def find_primes(start, end):
     primes = []
@@ -15,5 +13,15 @@ def find_primes(start, end):
             primes.append(num)
     return primes
 
+print("Prime Number Finder")
+try:
+    #input
+    user_start = int(input("Enter start value: "))
+    user_end = int(input("Enter end value: "))
 
-print(find_primes(1, 10))  # Output: [2, 3, 5, 7]
+    #output
+    result = find_primes(user_start, user_end)
+    print(f"Prime between {user_start} and {user_end}: {result}")
+
+except ValueError:
+    print("Enter whole numbers only")
