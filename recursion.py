580eff62328ba970f39e3eed790b392b959f37c7
0somesh <0somesh.sh@gmail.com>
2026-03-01 13:07:38 +0530

    update program

--- recursion.py
+++ recursion.py
@@ -4,6 +4,24 @@ def factorial(n):
     else:
         return n * factorial(n - 1)
 
-number = 9    ## input number
-result = factorial(number)
-print(f"The factorial of {number} is {result}")
+def start_program():
+    user_input = input("Enter a number to find its factorial ('exit' to quit): ")
+
+    if user_input.lower() == 'exit': #base case
+        print("end progeram")
+        return
+    try:
+        number = int(user_input)
+        if number < 0:
+            print("enter +ve integer")
+        else:
+            result = factorial(number)
+            print(f"factorial of {number} is {result}")
+    except ValueError:
+        print("invalid input! Please enter a whole number")
+
+    #recursive step
+    print("-" * 20)
+    start_program()
+
+start_program()
