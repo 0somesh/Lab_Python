d6e7c5d06f92aee25bd55cdba63639b20cc5f9b6
0somesh <0somesh.sh@gmail.com>
2026-03-28 15:45:35 +0530

    Array

---
+++ ArrayToOrdinary.py
@@ -0,0 +1,11 @@
+### QUES - Convert an Array to an Ordinary List with the Same Items 
+
+# ask??????????
+from array import *
+array_num = array('i', [1, 3, 57, 1, 9, 3])
+print("Original array: "+str(array_num))
+num_list = array_num.tolist()
+print("Convert the said array to an ordinary list with the same items:")
+print(num_list)
+
+
---
+++ RverseArray.py
@@ -0,0 +1,10 @@
+### QUES - everse the Order of Items in an Array
+
+
+
+from array import *
+array_num = array('i', [15, 35, 555, 213, 57, 1, 0, 99])
+print("Original array: "+str(array_num))
+array_num.reverse()
+print("Reverse the order of the items:")
+print(str(array_num))
